da2b6690ea25ee9920fc755ce60c576b73615fa5
seddam bayramov <bseddam@gmail.com>
2019-11-22 12:34:39 +0400

    ilk fayl

---
+++ jscreate/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace javascriptcreate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.javascript = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // javascript
+            // 
+            this.javascript.Location = new System.Drawing.Point(58, 34);
+            this.javascript.Name = "javascript";
+            this.javascript.Size = new System.Drawing.Size(75, 23);
+            this.javascript.TabIndex = 0;
+            this.javascript.Text = "js create";
+            this.javascript.UseVisualStyleBackColor = true;
+            this.javascript.Click += new System.EventHandler(this.javascript_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(164, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Click et və gözlə";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 112);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(762, 368);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 539);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.javascript);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button javascript;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ jscreate/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using jscreate;
+
+namespace javascriptcreate
+{
+    public partial class Form1 : Form
+    {
+        Class2 klas = new Class2();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void javascript_Click(object sender, EventArgs e)
+        {
+
+            string root = @"C:\Documents and Settings\user\Рабочий стол\jscreate\poi";
+
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+            }
+
+            DataTable dtGruplar = klas.getdatatable(@"
+
+SELECT    UPPER(Replace( Replace(Replace(d.unvan,CHAR(13)+CHAR(10),''),CHAR(34),CHAR(39)),'\','/'))  as dskunvan,CONVERT(varchar,d.dsk_n) +' '+ UPPER(Replace( Replace( Replace(d.dsk_ad,CHAR(13)+CHAR(10),''),CHAR(34),CHAR(39)),'\','/')) as dsk_ad,
+d.ggx,d.ggy,d.dsk_n,m.mnt_n,m.ggx as mggx ,m.ggy as mggy,m.mnt_nv,
+CONVERT(varchar,m.mnt_n) +N' SAYLI SEÇKİ MƏNTƏQƏSİ <br> '+ UPPER(Replace(Replace(Replace(m.unvan,CHAR(13)+CHAR(10),'') ,CHAR(34),CHAR(39)),'\','/')) as menunvan,
+CONVERT(varchar,m.mnt_n) +N' SAYLI SEÇKİ MƏNTƏQƏSİ' as seckimen
+
+FROM         daire as d INNER JOIN 
+                menteqe as m ON d.dsk_n = m.dsk_n 
+         where (m.ggx <> '0' or m.ggy <> '0')  and m.mnt_nv=1 
+         order by dsk_n,mnt_n
+");
+
+            string s1 = "";
+            
+            for (var i = 0; i < dtGruplar.Rows.Count; i++)
+            {
+                if (i != 0)
+                {
+                    if (dtGruplar.Rows[i - 1]["dsk_n"].ToString() != dtGruplar.Rows[i]["dsk_n"].ToString())
+                    {
+                        s1 = "";
+                    }
+                }
+                s1 = s1 + System.Environment.NewLine + "{" + '"' + "id" + '"' + ":" + '"' + dtGruplar.Rows[i]["mnt_n"].ToString() + '"'+","+
+                   System.Environment.NewLine + '"' + "x" + '"' + ":" + '"' + dtGruplar.Rows[i]["mggx"].ToString() + '"' + "," +
+                   System.Environment.NewLine + '"' + "y" + '"' + ":" + '"' + dtGruplar.Rows[i]["mggy"].ToString() + '"' + "," +
+                   System.Environment.NewLine + '"' + "nm" + '"' + ":" + '"' + dtGruplar.Rows[i]["seckimen"].ToString() + '"' + "," +
+                   System.Environment.NewLine + '"' + "addr" + '"' + ":" + '"' + dtGruplar.Rows[i]["menunvan"].ToString() + '"' + "},";
+                if (i != 0 && i != dtGruplar.Rows.Count-1)
+                {
+                    if (dtGruplar.Rows[i ]["dsk_n"].ToString() != dtGruplar.Rows[i+1]["dsk_n"].ToString())
+                    {
+                        s1 = s1.Substring(0, s1.Length-1);
+                        richTextBox1.Text = s1;   
+                    }
+                }
+                else if (i != 0 && i == dtGruplar.Rows.Count-1)
+                {
+                    s1 = s1.Substring(0, s1.Length-1);
+                }
+                TextWriter tw = new StreamWriter(@"C:\Documents and Settings\user\Рабочий стол\jscreate\poi\" + dtGruplar.Rows[i]["dsk_n"] + ".js");
+                tw.WriteLine("{" + '"' + "Number" + '"' + ":" + '"' + dtGruplar.Rows[i]["dsk_n"].ToString() + '"' + ',' +
+         System.Environment.NewLine + '"' + "x" + '"' + ":" + '"' + dtGruplar.Rows[i]["ggx"].ToString() + '"'+"," +
+         System.Environment.NewLine + '"' + "y" + '"' + ":" + '"' + dtGruplar.Rows[i]["ggy"].ToString() + '"' + "," +
+         System.Environment.NewLine + '"' + "z" + '"' + ":" + '"' + "3" + '"' + "," +
+         System.Environment.NewLine + '"' + "nm" + '"' + ":" + '"' + dtGruplar.Rows[i]["dsk_ad"].ToString() + '"' + "," +
+         System.Environment.NewLine + '"' + "addr" + '"' + ":" + '"' + dtGruplar.Rows[i]["dsk_ad"].ToString() + " <br> " + dtGruplar.Rows[i]["dskunvan"].ToString() + '"' + "," +
+         System.Environment.NewLine + '"' + "rows" + '"' + ":[" + s1   + System.Environment.NewLine+ "]}");
+
+                tw.Close();
+            
+            }
+
+            label1.Text = "Başa çatdı";
+
+          
+
+
+
+            //FileStream fs2 = new FileStream("D:\\Yourfile.js", FileMode.OpenOrCreate, FileAccess.Read);
+            //StreamReader reader = new StreamReader(fs2);
+            //textBox1.Text = reader.ReadToEnd();
+            //reader.Close();
+        }
+    }
+}
